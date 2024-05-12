4c91081e1c13c6f2828cfcc36203c050d027717f
Anupam Bhakta <anupambhakta1@gmail.com>
2024-05-12 15:11:11 -0500

    implemented optimize

--- randomwalk.py
+++ randomwalk.py
@@ -6,7 +6,7 @@ class RandomWalkOptimization:
     def __init__(self, graph, start, criterion):
         if graph.shape[0] != graph.shape[1]:
             raise ValueError("Graph adjacency matrix dimensions must be square.")
-        
+
         self.graph = graph
         self.start = start
         self.criterion = criterion
@@ -15,7 +15,19 @@ class RandomWalkOptimization:
         if criterion is None:
             criterion = self.criterion
 
-        pass
+        current_node = self.start
+        while True:
+            callback_result = callback(current_node)
+            if criterion(callback_result):
+                break
+
+            neighbors = np.where(self.graph[current_node] > 0)[0]
+            if len(neighbors) == 0:
+                break
+
+            current_node = random.choice(neighbors)
+
+        return current_node
 
     def normalize_graph(self):
         rowsums = self.graph.sum(axis=1)
