f1e97a0893fa359351d9cdcd011d420f80a61c4d
Anupam Bhakta <anupambhakta1@gmail.com>
2024-07-04 20:13:54 -0500

    Remove html_static_path from conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,4 +31,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_book_theme"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
