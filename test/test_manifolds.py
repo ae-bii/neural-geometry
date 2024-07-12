b431ee3f078f9534e0ac6f873e1dc1c7edd9392b
Kevin Qiu <kzqiu01@gmail.com>
2024-07-11 19:11:59 -0700

    Initial test examples.

--- test/test_manifolds.py
+++ test/test_manifolds.py
@@ -5,8 +5,7 @@ def test_product_manifold():
     """
     nlgm.manifolds.ProductManifold
     """
-    m = ProductManifold([1])
-    assert m is not None
+    assert ProductManifold([1]) is not None
 
 
 if __name__ == "__main__":
--- test/test_searchspace.py
+++ test/test_searchspace.py
@@ -1,11 +1,18 @@
 from nlgm.searchspace import manifold_to_curvature
+import pytest
 
 
 def test_manifold_to_curvature():
     """
     nlgm.searchspace.manifold_to_curvature
     """
-    assert manifold_to_curvature("E2") == 0
+    map = {"E2": 0, "S2": 1, "H2": -1}
+
+    for manifold, curvature in map.items():
+        assert manifold_to_curvature(manifold) == curvature
+
+    with pytest.raises(ValueError):
+        manifold_to_curvature("H5")
 
 
 if __name__ == "__main__":
