036562c762d1c297ec30cfccdbf7843d00bbc3b3
ae-bii <ae-bii@users.noreply.github.com>
2024-07-13 02:40:21 +0000

    deploy: 19ce50eb558f3a21b8962c3b7286883baf6244b7

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,4 +1,5 @@
-const DOCUMENTATION_OPTIONS = {
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '0.1.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
@@ -6,8 +7,8 @@ const DOCUMENTATION_OPTIONS = {
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '',
-    NAVIGATION_WITH_KEYS: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
